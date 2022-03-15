ffc8cd25ced6aedf97b7b04499f0a48cb5df2585
Dipon Barua <85799541+Dipon-Ctg@users.noreply.github.com>
2022-03-15 11:42:17 +0100

    Add files via upload

---
+++ Data Library.py
@@ -0,0 +1,21 @@
+"""
+studentID= {
+    101: "Dipon Barua",
+    102: "Suraj",
+    103: "Asif",
+
+}
+n=int(input("Enter your Matriculation Number:"))
+n=studentID
+print(n)
+
+101: "Sip"
+102: "Dio"
+"""
+
+
+n=int(input("Enter:"))
+if n==101:
+    print("Dip")
+elif n==102:
+    print("Raj")
